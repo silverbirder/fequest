ed6011b8b681a71f99ada93d62361c8c9ddb0212
shibamoto <silverbirder@gmail.com>
2025-11-30 22:07:03 +0900

    refactor: remove unused dependencies and clean up configuration

--- packages/storybook/.storybook/vitest.setup.ts
+++ packages/storybook/.storybook/vitest.setup.ts
@@ -1,15 +1,8 @@
 import * as a11yAddonAnnotations from "@storybook/addon-a11y/preview";
 import { setProjectAnnotations } from "@storybook/nextjs-vite";
-import { screenshot } from "@storycap-testrun/browser";
-import { afterEach } from "vitest";
-import { page } from "vitest/browser";
 
 import * as projectAnnotations from "./preview";
 
 // This is an important step to apply the right configuration when testing your stories.
 // More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
 setProjectAnnotations([a11yAddonAnnotations, projectAnnotations]);
-
-afterEach(async (context) => {
-  await screenshot(page, context, {});
-});
